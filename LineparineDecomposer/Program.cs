a8fe48ee91d09b592a2026cb58cef5f38d8ed147
skytomo <tomo1kawa.fullerene@gmail.com>
2020-06-14 00:47:20 +0900

    Add lineparine decomposer

---
+++ LineparineDecomposer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LineparineDecomposer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ld = new LineparineDecomposer();
+            var ans = ld.Decompose("misse's");
+            Console.WriteLine(string.Join(" ", ans[0]));
+        }
+    }
+}
