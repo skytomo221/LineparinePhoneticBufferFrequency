2df1a9e7301848bbfd9d5f20077ce49f51263a5e
skytomo <tomo1kawa.fullerene@gmail.com>
2020-07-01 15:52:30 +0900

    Implement eulaqunesykaxm generator

--- EulaqunesykaxmGenerator/Program.cs
+++ EulaqunesykaxmGenerator/Program.cs
@@ -1,5 +1,7 @@
-﻿using Otamajakushi;
+﻿using LineparinePhoneticBufferFrequency;
+using Otamajakushi;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -13,10 +15,66 @@ namespace EulaqunesykaxmGenerator
                 from word in OneToManyJsonSerializer.Deserialize(File.ReadAllText(@"dictionary.json")).Words
                 select word.Entry.Form;
             using (StreamReader sr = new StreamReader("input.txt"))
-            using (StreamWriter swBuffer = new StreamWriter("buffer.tsv"))
-            using (StreamWriter swAll = new StreamWriter("all.tsv"))
-            using (StreamWriter swExample = new StreamWriter("example.tsv"))
+            using (StreamReader srData = new StreamReader("../../../Data.tsx"))
+            using (StreamWriter sw = new StreamWriter("output.tsx"))
             {
+                string data = srData.ReadToEnd();
+                var text = sr.ReadToEnd();
+                var words =
+                   from word in text.Split('\n')
+                   .Distinct()
+                   where !dictionary.Contains(word)
+                   select word;
+                var analyzer = new LineparinePhoneticBufferFrequencyAnalyzer
+                {
+                    Text = text,
+                    Words = words,
+                };
+                var table = analyzer.Analyze();
+                var alphabet = new List<string> {
+                    "i", "y", "u", "o", "e", "a",
+                    "p", "fh", "f", "t", "c", "x",
+                    "k", "q", "h", "r", "z", "m",
+                    "n", "l", "j", "w", "b",
+                    "vh", "v", "d", "s", "g", "dz",
+                    "ph", "ts", "ch", "ng", "sh",
+                    "th", "dh", "kh", "rkh", "rl",
+                };
+                sw.WriteLine(data.Trim());
+                sw.WriteLine("export const rates : {[key: string]: number | null} = {");
+                foreach (var left in alphabet)
+                {
+                    foreach (var right in alphabet)
+                    {
+                        var tuple = new Tuple<string, string>(left, right);
+                        if (table.ContainsKey(tuple))
+                        {
+                            var rate = ((double)(table[tuple].Buffer + 1) / (table[tuple].All + 2));
+                            sw.WriteLine($"['{left}+{right}'] : {((table[tuple].All == 0) ? "null" : rate.ToString())},");
+                        }
+                        else
+                        {
+                            sw.WriteLine($"['{left}+{right}'] : null,");
+                        }
+                    }
+                }
+                sw.WriteLine("}");
+                sw.WriteLine("export const examples : { [key: string]: Array<Example> } = {");
+                foreach (var left in alphabet)
+                {
+                    foreach (var right in alphabet)
+                    {
+                        var tuple = new Tuple<string, string>(left, right);
+                        if (table.ContainsKey(tuple))
+                        {
+                            var example = table[tuple].Examples
+                                .Select(w => $"{{\nword : \"{w.Word}\",\nparts : [{string.Join(", ", w.Decomposition.Select(d => $"\"{d}\""))}],\nleftIndex : {w.FastLetterIndex},\nrightIndex : {w.LastLetterIndex}\n}}")
+                                .Aggregate((now, next) => $"{now},\n{next}");
+                            sw.WriteLine($"['{left}+{right}'] : [\n{example}\n],");
+                        }
+                    }
+                }
+                sw.WriteLine("}");
             }
         }
     }
--- EulaqunesykaxmGenerator/data.tsx
+++ EulaqunesykaxmGenerator/data.tsx
@@ -1,5 +1,5 @@
-﻿const vowels: Array<Vowel> = ['i', 'y', 'u', 'o', 'e', 'a'];
-const consonants: Array<Consonant> = [
+﻿export const vowels: Array<Vowel> = ['i', 'y', 'u', 'o', 'e', 'a'];
+export const consonants: Array<Consonant> = [
     'p', 'fh', 'f', 't', 'c', 'x',
     'k', 'q', 'h', 'r', 'z', 'm',
     'n', 'r', 'l', 'j', 'w', 'b',
@@ -7,19 +7,34 @@ const consonants: Array<Consonant> = [
     'ph', 'ts', 'ch', 'ng', 'sh',
     'th', 'dh', 'kh', 'rkh', 'rl'
 ];
-type Vowel = 'i' | 'y' | 'u' | 'o' | 'e' | 'a';
-type Consonant =
+export const letters: Array<Letter> = [
+    "i", "y", "u", "o", "e", "a",
+    "p", "fh", "f", "t", "c", "x",
+    "k", "q", "h", "r", "z", "m",
+    "n", "r", "l", "j", "w", "b",
+    "vh", "v", "d", "s", "g", "dz",
+    "ph", "ts", "ch", "ng", "sh",
+    "th", "dh", "kh", "rkh", "rl",
+];
+export type Vowel = 'i' | 'y' | 'u' | 'o' | 'e' | 'a';
+export type Consonant =
     'p' | 'fh' | 'f' | 't' | 'c' | 'x' |
     'k' | 'q' | 'h' | 'r' | 'z' | 'm' |
     'n' | 'r' | 'l' | 'j' | 'w' | 'b' |
     'vh' | 'v' | 'd' | 's' | 'g' | 'dz' |
     'ph' | 'ts' | 'ch' | 'ng' | 'sh' |
     'th' | 'dh' | 'kh' | 'rkh' | 'rl';
-type Letter = Vowel | Consonant;
-interface SoundPair {
+export type Letter = Vowel | Consonant;
+export interface SoundPair {
     left: Letter;
     right: Letter;
 }
-interface ExampleDictionary {
+export interface ExampleDictionary {
     [id: string]: Array<string>;
+}
+export interface Example {
+    word: string;
+    parts: Array<string>;
+    leftIndex: number;
+    rightIndex: number;
 }
